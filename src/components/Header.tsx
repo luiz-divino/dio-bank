640a190628d091bce05b66ac48976087c2329348
luizdivino <nandodivinodev@gmail.com>
2026-02-02 16:34:00 -0300

    componente header que leva apenas o titulo do projeto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
+import { ChakraProvider, defaultSystem, Box, Center } from "@chakra-ui/react";
 
 export const Header = () => {
-    return (
-        <div>
-            Dio bank
-        </div>
-    )
+  return <ChakraProvider value={defaultSystem}>
+    <Box >
+        <Center backgroundColor={'#F54927'} fontSize={'2xl'} top={'0'} padding={'10px'} borderRadius={'md'} color={'white'}>
+            DIO BANK
+        </Center>
+    </Box>
+  </ChakraProvider>;
 };
